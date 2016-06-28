93af0caf390446e724f8aefaa72e195e047b7e87
PIARDSylvian <piard.sylvian@gmail.com>
2016-06-28 14:31:01 +0200

    add layout, all for Dashboard-GED.

--- src/CS/GedBundle/Controller/DefaultController.php
+++ src/CS/GedBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('GedBundle:Default:index.html.twig');
+        return $this->render('GedBundle::index.html.twig');
     }
 }
