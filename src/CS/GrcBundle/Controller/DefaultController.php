92bed8c1262e589189c0061e4ee200f1ef455249
julianxiaoyu <julianmoreau@yahoo.fr>
2016-07-08 11:38:54 +0200

    create ticket ticket display

--- src/CS/GrcBundle/Controller/DefaultController.php
+++ src/CS/GrcBundle/Controller/DefaultController.php
@@ -41,6 +41,9 @@ class DefaultController extends Controller
             $ticket->setIdcategory($cat);
             $ticket->setIdsouscategory($sscat);
 
+            $ticket->setStatus("Initial");
+            $ticket->setPriority("Not defined");
+
             $em->persist($ticket);
             $em->flush();
 
@@ -54,11 +57,79 @@ class DefaultController extends Controller
         ));
     }
 	
-	public function ticketAction(Request $request)
+	public function ticketAction(Request $request, Ticket $ticket)
 	{
-	
+
 	$em = $this->getDoctrine()->getManager();
-	return $this->render('GrcBundle:Default:ticket.html.twig');
+    $idcat= $ticket->getIdcategory();
+    $idsscat= $ticket->getIdsouscategory();
+    $categorie = $em->getRepository('GrcBundle:Grccategory')->findOneById($idcat);
+    $sscategorie = $em->getRepository('GrcBundle:Grcsouscategory')->findOneById($idsscat);
+  
+    return $this->render('GrcBundle:Default:ticket.html.twig', array(
+        'ticket'=>$ticket,
+        'categorie'=>$categorie,
+        'sscategorie'=>$sscategorie,
+        'idduticket'=>$idduticket,
+        ));
+
 
 	}
+    public function listeAction(Request $request)
+    {
+    
+    $em = $this->getDoctrine()->getManager();
+    $user = $this->container->get('security.context')->getToken()->getUser();
+    $iduser=$user->getId();
+    
+    $alltickets=$em->getRepository('GrcBundle:Ticket')->findAll();
+
+    foreach ($alltickets as $ticket) {
+            //recuperer l'ID du ticket
+            $idticket=$ticket->getId();    
+            //recuperer l'ID du ticket
+            $iddemandeur=$ticket->getIdsender(); 
+            //recuperer la date de creation du ticket
+            $dateticket=$ticket->getDate();
+            //recuperer la priorité
+            $prioticket=$ticket->getPriority();
+            //recuperer le status
+            $statusticket=$ticket->getStatus();
+            //recuperer l'ID catégorie
+            $idcategory=$ticket->getIdcategory();
+            //recuperer l'ID sous-catégorie
+            $idsscategory=$ticket->getIdsouscategory();
+            
+            //allez chercher la catégorie 
+            $cat = $em->getRepository('GrcBundle:Grccategory')->findOneById($idcategory);
+            $catname=$cat->getName();
+            
+            //allez chercher la sous-catégorie 
+            $sscat = $em->getRepository('GrcBundle:Grcsouscategory')->findOneById($idsscategory);
+            $sscatname=$sscat->getName();
+
+            if(empty($tagnames))
+            {
+                $tagnames=1;
+            }
+            $ticketslist[]=array(
+                "idticket"=>$idticket,
+                "catname"=>$catname,
+                "sscatname"=>$sscatname,
+                "iddemandeur"=>$iddemandeur,
+                "dateticket"=>$dateticket,
+                "prioticket"=>$prioticket,
+                "statusticket"=>$statusticket
+                );
+        }
+        if(empty($tabfav))
+        {
+            $tabfav=1;
+        }
+
+    return $this->render('GrcBundle:Default:liste.html.twig', array(
+        'ticketslist'=>$ticketslist,
+        ));
+
+    }
 }
