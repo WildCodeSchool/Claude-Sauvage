4e9ef1267f2ff353db7591c434c0846bc0356592
Gugusteh <augustin.lefebvre83@gmail.com>
2016-07-11 13:46:51 +0200

    ajout dyna dashboard grc

---
+++ src/CS/GrcBundle/Controller/ListController.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace CS\GrcBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use CS\GrcBundle\Entity\Ticket;
+use CS\GrcBundle\Form\TicketType;
+use DateTime;
+
+class ListController extends Controller
+{
+	public function listeAction(Request $request)
+    {
+    
+    $em = $this->getDoctrine()->getManager();
+    $user = $this->container->get('security.context')->getToken()->getUser();
+    $iduser=$user->getId();
+    
+    $alltickets=$em->getRepository('GrcBundle:Ticket')->findAll();
+
+    foreach ($alltickets as $ticket) {
+            //recuperer l'ID du ticket
+            $idticket=$ticket->getId();    
+            //recuperer l'ID du ticket
+            $iddemandeur=$ticket->getIdsender(); 
+            //recuperer la date de creation du ticket
+            $dateticket=$ticket->getDate();
+            //recuperer la priorité
+            $prioticket=$ticket->getPriority();
+            //recuperer le status
+            $statusticket=$ticket->getStatus();
+            //recuperer l'ID catégorie
+            $idcategory=$ticket->getIdcategory();
+            //recuperer l'ID sous-catégorie
+            $idsscategory=$ticket->getIdsouscategory();
+            
+            //allez chercher la catégorie 
+            $cat = $em->getRepository('GrcBundle:Grccategory')->findOneById($idcategory);
+            $catname=$cat->getName();
+            
+            //allez chercher la sous-catégorie 
+            $sscat = $em->getRepository('GrcBundle:Grcsouscategory')->findOneById($idsscategory);
+            $sscatname=$sscat->getName();
+
+            if(empty($tagnames))
+            {
+                $tagnames=1;
+            }
+            $ticketslist[]=array(
+                "idticket"=>$idticket,
+                "catname"=>$catname,
+                "sscatname"=>$sscatname,
+                "iddemandeur"=>$iddemandeur,
+                "dateticket"=>$dateticket,
+                "prioticket"=>$prioticket,
+                "statusticket"=>$statusticket
+                );
+        }
+        if(empty($tabfav))
+        {
+            $tabfav=1;
+        }
+        if (empty($ticketslist))
+        {
+          $ticketslist=1;
+        }
+
+    return $this->render('GrcBundle:Default:liste.html.twig', array(
+        'ticketslist'=>$ticketslist,
+        ));
+
+    }
+    public function showDashboardAction (Request $request)
+    {
+    	$em=$this->getDoctrine()->getManager();
+    	$user=$this->getUser();
+
+    	$newtickets=$em->getRepository('GrcBundle:Ticket')->findBy(
+    		array('idreceiver' => $user->getId()), // Critere
+  			array('date' => 'desc'),        // Tri
+  			5,                              // Limite
+  			0                               // Offset
+    	);
+    	foreach ($newtickets as $ticket)
+    	{
+    		$ticketid = $ticket->getId();
+    		$date=$ticket->getDate();
+    		$priority=$ticket->getPriority();
+
+    		$sender= $em->getRepository('AppBundle:User')->findOneById($ticket->getIdsender());
+    		$ticketsender=$sender->getUsername();
+
+    		$category=$em->getRepository('GrcBundle:Grccategory')->findOneById($ticket->getIdcategory());
+    		$categoryname=$category->getName();
+
+    		$newticketstab[]=array(
+    			'id'=>$ticketid,
+    			'date'=>$date,
+    			'priority'=>$priority,
+    			'sender'=>$ticketsender,
+    			'category'=>$categoryname,
+    			);
+    	}
+    	if(empty($newticketstab))
+    	{
+    		$newticketstab=1;
+    	}
+
+    	$highlyurgenttickets=$em->getRepository('GrcBundle:Ticket')->findBy(
+    		array('idreceiver'=>$user->getId(), 'priority'=>'Très urgent'),
+    		array('date'=>'desc'),
+    		5,
+    		0
+    		);
+    	foreach ($highlyurgenttickets as $huticket) {
+    		
+    		$ticketid = $ticket->getId();
+    		$date=$ticket->getDate();
+    		$priority=$ticket->getPriority();
+
+    		$sender= $em->getRepository('AppBundle:User')->findOneById($ticket->getIdsender());
+    		$ticketsender=$sender->getUsername();
+
+    		$category=$em->getRepository('GrcBundle:Grccategory')->findOneById($ticket->getIdcategory());
+    		$categoryname=$category->getName();
+
+    		$huticketstab[]=array(
+    			'id'=>$ticketid,
+    			'date'=>$date,
+    			'priority'=>$priority,
+    			'sender'=>$ticketsender,
+    			'category'=>$categoryname,
+    			);
+    	}
+	   	if(empty($huticketstab))
+    	{
+    		$huticketstab=1;
+    	}
+
+    	$urgenttickets=$em->getRepository('GrcBundle:Ticket')->findBy(
+    		array('idreceiver'=>$user->getId(), 'priority'=>'Urgent'),
+    		array('date'=>'desc'),
+    		5,
+    		0
+    		);
+    	foreach ($urgenttickets as $urgentticket) {
+    		
+    		$ticketid = $ticket->getId();
+    		$date=$ticket->getDate();
+    		$priority=$ticket->getPriority();
+
+    		$sender= $em->getRepository('AppBundle:User')->findOneById($ticket->getIdsender());
+    		$ticketsender=$sender->getUsername();
+
+    		$category=$em->getRepository('GrcBundle:Grccategory')->findOneById($ticket->getIdcategory());
+    		$categoryname=$category->getName();
+
+    		$urgentticketstab[]=array(
+    			'id'=>$ticketid,
+    			'date'=>$date,
+    			'priority'=>$priority,
+    			'sender'=>$ticketsender,
+    			'category'=>$categoryname,
+    			);
+    	}
+    	if(empty($urgentticketstab))
+    	{
+    		$urgentticketstab=1;
+    	}
+
+    	$comments=$em->getRepository('GrcBundle:Comment')->findAll(
+    		array('date'=>'desc')
+    		);
+    	$compteur=0;
+    	$tab[]=array('id'=>0);
+    	foreach ($comments as $comment ) {
+    		$counted=0;
+    		$i=0;
+    		$idticket=$comment->getIdticket();
+    		while($i<count($tab))
+    		{
+    			if( $idticket = $tab[$i]['id'] )
+    			{
+    				$counted=1;
+    			}
+    			$i++;
+    		}
+    		if($counted == 0 && $compteur < 5 )
+    		{
+    			$ticket= $em->getRepository('GrcBundle:Ticket')->findOneById($idticket);
+
+    			$ticketid = $ticket->getId();
+    			$date=$ticket->getDate();
+    			$priority=$ticket->getPriority();
+
+    			$sender= $em->getRepository('AppBundle:User')->findOneById($ticket->getIdsender());
+    			$ticketsender=$sender->getUsername();
+
+    			$category=$em->getRepository('GrcBundle:Grccategory')->findOneById($ticket->getIdcategory());
+    			$categoryname=$category->getName();
+
+    			$commentedticketstab[]=array(
+    				'id'=>$ticketid,
+    				'date'=>$date,
+    				'priority'=>$priority,
+    				'sender'=>$ticketsender,
+    				'category'=>$categoryname,
+    			);
+    		}
+    	}
+    	if(empty($commentedticketstab))
+    	{
+    		$commentedticketstab=1;
+    	}
+    	return $this->render('GrcBundle:Default:index.html.twig', array(
+    		'newtickets'=>$newticketstab,
+    		'hutickets'=>$huticketstab,
+    		'urgenttickets'=>$urgentticketstab,
+    		'commentedtickets'=>$commentedticketstab,
+    		));
+    }
+}
