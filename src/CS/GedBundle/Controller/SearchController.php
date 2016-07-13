d81646b1c843e6975ca729c730d3232ef565ab46
PIARDSylvian <piard.sylvian@gmail.com>
2016-07-13 11:09:29 +0200

    Fix#5 list sscatégory disabled

--- src/CS/GedBundle/Controller/SearchController.php
+++ src/CS/GedBundle/Controller/SearchController.php
@@ -40,260 +40,39 @@ class SearchController extends Controller
 
     public function searchAction(Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
+        $em=$this->getDoctrine()->getManager();
         $user =$this->getUser();
 
-        $content = $this->getRequest()->request->get('Recherche');
-        $Catégorie = $this->getRequest()->request->get('Catégorie');
-        $SSCatégorie = $this->getRequest()->request->get('SSCatégorie');
-        $type = $this->getRequest()->request->get('Type');
+        $searchRecherche=$request->request->get('recherche');
+        $searchCategorie=$request->request->get('categorie');
+        $searchSscategories=$request->request->get('sscategories');
+        $searchType=$request->request->get('type');
 
-        // définition plus tard du la recherche de type de fichier.
-
-        if($SSCatégorie != 0){
-
-            //recherche mes fichier.
-
-            $Myfiles = $em->getRepository('GedBundle:Gedfiles')->findBy( array(
-                                                                                'idsouscategory'=>$SSCatégorie,
-                                                                                'idowner'=>$user->getId(),
-                                                                            ));
-
-            foreach ($Myfiles as $Myfile)
-            {
-                $type=$Myfile->getType();
-                $path=$Myfile->getPath();
-                $idfile=$Myfile->getId();
-                $date=$Myfile->getDate();
-                $name=$Myfile->getOriginalName();
-
-                $linktag = $em->getRepository('GedBundle:Linktag')->findByIdfile($idfile);
-                foreach ($linktag as $tag) {
-                    //on recupere l'id du premier tag
-                    $idtag=$tag->getIdtag();
-                    //on recupere la ligne de la table Gedtag correspondante à l'id d'au dessus
-                    $infostag=$em->getRepository('GedBundle:Gedtag')->findOneById($idtag);
-                    //on recupere le nom du tag et on met tout ca dans un tableau
-                    $tagname=$infostag->getName();
-                    $tagnames[]=array(
-                        'id'=>$idtag,
-                        'name'=>$tagname,
-                        );
-                
-                //on fout tout dans un tableau et on a des favoris tout neufs
-                }
-                if(empty($tagnames))
-                {
-                    $tagnames=1;
-                }
-
-                //récuperation des favoris.
-                $bookmarkfile = $em->getRepository('GedBundle:Linkbookmark')->findOneByIdfile($Myfile->getId());
-
-                if (empty($bookmarkfile)){
-                $bookmarkfile = 0;
-                }
-
-                else{
-                    $bookmarkfile = 1;
-                }
-
-                //on compte les commentaires liés a un fichier.
-                $comments =$em->getRepository('GedBundle:Gedcom')->findByIdfile($Myfile->getId());
-                if (empty($comments)){
-                        $nbCom = 0;
-                    }
-                else {
-                    $nbCom = count($comments);
-                }
-
-                $tabFiles[]=array(
-                    "idfile"=>$idfile,
-                    "tagnames"=>$tagnames,
-                    "path"=>$path,
-                    "type"=>$type,
-                    "category"=>$SSCatégorie,
-                    "date"=>$date,
-                    "name"=>$name
-                    "bookmark"=>$bookmarkfile,
-                    "com"=>$nbCom,
-                );
-            }
-
-             //recherche les fichier de mon / mes groupes.
-
-            $listgroups=$em->getRepository('GedBundle:Linkgroup')->findByIduser($user->getId());
-
-            foreach ($listgroups as $listgroup)
-            {
-                $grpId= $listgroup->getIdgroup();
-                $grpFiles=$em->getRepository('GedBundle:Gedfiles')->findByIdgroup($grpId);
-
-                foreach ($grpFiles as $grpFile)
-                {
-                    $type=$grpFile->getType();
-                    $path=$grpFile->getPath();
-                    $idfile=$grpFile->getId();
-                    $date=$grpFile->getDate();
-                    $name=$grpFile->getOriginalName();
-
-                    $linktag = $em->getRepository('GedBundle:Linktag')->findByIdfile($idfile);
-                    foreach ($linktag as $tag) {
-                        //on recupere l'id du premier tag
-                        $idtag=$tag->getIdtag();
-                        //on recupere la ligne de la table Gedtag correspondante à l'id d'au dessus
-                        $infostag=$em->getRepository('GedBundle:Gedtag')->findOneById($idtag);
-                        //on recupere le nom du tag et on met tout ca dans un tableau
-                        $tagname=$infostag->getName();
-                        $tagnames[]=array(
-                            'id'=>$idtag,
-                            'name'=>$tagname,
-                            );
-                        //on fout tout dans un tableau et on a des favoris tout neufs
-                    }
-                    if(empty($tagnames))
-                    {
-                        $tagnames=1;
-                    }                
-
-                    //récuperation des favoris.
-                    $bookmarkfile = $em->getRepository('GedBundle:Linkbookmark')->findOneByIdfile($myfile->getId());
-
-                    if (empty($bookmarkfile)){
-                    $bookmarkfile = 0;
-                    }
-
-                    else{
-                        $bookmarkfile = 1;
-                    }
-
-                    //on compte les commentaires liés a un fichier.
-                    $comments =$em->getRepository('GedBundle:Gedcom')->findByIdfile($Myfile->getId());
-                    if (empty($comments)){
-                        $nbCom = 0;
-                    }
-                    else {
-                        $nbCom = count($comments);
-                    }
-
-                    $tabGrpFiles[]=array(
-                        "idfile"=>$idfile,
-                        "tagnames"=>$tagnames,
-                        "path"=>$path,
-                        "type"=>$type,
-                        "category"=>$SSCatégorie,
-                        "date"=>$date,
-                        "name"=>$name,
-                        "bookmarkfile"=>$bookmarkfile,
-                        "com"=>$nbCom,
-                    );
-                }
-            }
-            return $this->render('GedBundle::result_search.html.twig',array(
-                                                                            'form' => $form->createView(),
-                                                                            'user'=>$user,
-                                                                            'tabFiles'=> $tabFiles,
-                                                                            'tabGrpFiles'=>$tabGrpFiles,
-                                                                        ));
-        }
-
-        elseif($Catégorie != 0){
+        //si pas de sous catégorie défini.
+        if (empty($searchSscategories)||($searchSscategories==0)){
             
-            //recherche mes fichier.
-
-            $Myfiles = $em->getRepository('GedBundle:Gedfiles')->findBy( array(
-                                                                                        'idcategory'=>$Catégorie,
-                                                                                        'idowner'=>$user->getId(),
-                                                                            ))  ;
-
-            foreach ($Myfiles as $Myfile)
-            {
-                $type=$Myfile->getType();
-                $path=$Myfile->getPath();
-                $idfile=$Myfile->getId();
-                $date=$Myfile->getDate();
-                $name=$Myfile->getOriginalName();
-
-                $linktag = $em->getRepository('GedBundle:Linktag')->findByIdfile($idfile);
-                foreach ($linktag as $tag) {
-                    //on recupere l'id du premier tag
-                    $idtag=$tag->getIdtag();
-                    //on recupere la ligne de la table Gedtag correspondante à l'id d'au dessus
-                    $infostag=$em->getRepository('GedBundle:Gedtag')->findOneById($idtag);
-                    //on recupere le nom du tag et on met tout ca dans un tableau
-                    $tagname=$infostag->getName();
-                    $tagnames[]=array(
-                        'id'=>$idtag,
-                        'name'=>$tagname,
-                        );
-                    //on fout tout dans un tableau et on a des favoris tout neufs
-                }
-                if(empty($tagnames))
-                {
-                    $tagnames=1;
-                }
-
-                $tabFiles[]=array(
-                    "idfile"=>$idfile,
-                    "tagnames"=>$tagnames,
-                    "path"=>$path,
-                    "type"=>$type,
-                    "category"=>$Catégorie,
-                    "date"=>$date,
-                    "name"=>$name
-                );
+            //on recherche alors si la catégorie et défini.
+            if ($searchCategorie!=0){
+                echo $searchCategorie;
             }
-
-             //recherche les fichier de mon / mes groupes.
-
-            $listgroups=$em->getRepository('GedBundle:Linkgroup')->findByIduser($user->getId());
-
-            foreach ($listgroups as $listgroup)
-            {
-                $grpId= $listgroup->getIdgroup();
-                $grpFiles=$em->getRepository('GedBundle:Gedfiles')->findByIdgroup($grpId);
-
-                foreach ($grpFiles as $grpFile)
-                {
-                    $type=$grpFile->getType();
-                    $path=$grpFile->getPath();
-                    $idfile=$grpFile->getId();
-                    $date=$grpFile->getDate();
-                    $name=$grpFile->getOriginalName();
-
-                    $linktag = $em->getRepository('GedBundle:Linktag')->findByIdfile($idfile);
-                    foreach ($linktag as $tag) {
-                        //on recupere l'id du premier tag
-                        $idtag=$tag->getIdtag();
-                        //on recupere la ligne de la table Gedtag correspondante à l'id d'au dessus
-                        $infostag=$em->getRepository('GedBundle:Gedtag')->findOneById($idtag);
-                        //on recupere le nom du tag et on met tout ca dans un tableau
-                        $tagname=$infostag->getName();
-                        $tagnames[]=array(
-                            'id'=>$idtag,
-                            'name'=>$tagname,
-                            );
-                        //on fout tout dans un tableau et on a des favoris tout neufs
-                    }
-                    if(empty($tagnames))
-                    {
-                        $tagnames=1;
-                    }
-
-                    $tabGrpFiles[]=array(
-                        "idfile"=>$idfile,
-                        "tagnames"=>$tagnames,
-                        "path"=>$path,
-                        "type"=>$type,
-                        "category"=>$Catégorie,
-                        "date"=>$date,
-                        "name"=>$name
-                    );
-                }
+            //Si la catégorie n'est pas défini alors.
+            else{
+                 echo'pas de catégorie & pas de sous catégorie';
             }
         }
 
+        //on obtien donc il de la sous catégorie.
         else{
+            echo $searchSscategories;
         }
-    }
+        var_dump($searchRecherche);
+        var_dump('--------------------------');
+        var_dump($searchCategorie);
+        var_dump('--------------------------');
+        var_dump($searchSscategories);
+        var_dump('--------------------------');
+        var_dump($searchType);exit;
+        
+    
+    }    
 }
