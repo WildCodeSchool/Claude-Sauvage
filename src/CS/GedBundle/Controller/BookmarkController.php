8163afba07a84bba31b67713760b37670f9b065c
PIARDSylvian <piard.sylvian@gmail.com>
2016-07-22 11:13:44 +0200

    start page bookmark

--- src/CS/GedBundle/Controller/BookmarkController.php
+++ src/CS/GedBundle/Controller/BookmarkController.php
@@ -5,8 +5,15 @@ namespace CS\GedBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use CS\GedBundle\Entity\Linkbookmark;
+use CS\GedBundle\Entity\Gedfiles;
+use CS\GedBundle\Form\GedfilesType;
 use AppBundle\Entity\User;
+use CS\GedBundle\Entity\Linkbookmark;
+use CS\GedBundle\Entity\Category;
+use CS\GedBundle\Entity\Souscategory;
+use CS\GedBundle\Entity\Linktag;
+use CS\GedBundle\Entity\Gedtag;
+use DateTime;
 
 class BookmarkController extends Controller
 {
@@ -44,5 +51,140 @@ class BookmarkController extends Controller
         $response = new JsonResponse();
 
         return $response->setData(array('response' => $verifFav));
+    }
+    public function bookmarkListAction(Request $request)
+    {
+        //récuperation & atribution de l entitiy manager.
+        $em=$this->getDoctrine()->getManager();
+
+        //récuperation de l'utilisateur courant.
+        $user =$this->getUser();
+        $iduser=$user->getId();
+
+        $categories = $em->getRepository('GedBundle:Category')->findAll();
+
+        //récuperation des sous-catégories.
+        $categoryTab = [];
+        foreach ($categories as $category) {
+
+            $categoryInfos = $em->getRepository('GedBundle:Souscategory')->findByIdcategory($category->getId());
+
+            if (!empty($categoryInfos)){
+
+                //On place les sous-catégorie dans un tableau si elle sont définie.
+                foreach ($categoryInfos as $categoryInfo) {
+
+                    $categoryName=$categoryInfo->getName();
+                    $categoryId=$categoryInfo->getIdcategory();
+            
+                    $categoryTab[] = array(
+                        'category' => $categoryName,
+                        'id' => $categoryId,
+                    );
+                }
+            }
+        }
+
+        //création d'une nouvelle instance de l'entité Gedfiles.
+        $gedfiles = new Gedfiles();
+
+        //créetion du formulaire
+        $form = $this->createForm(GedfilesType::class, $gedfiles);
+        $form->handleRequest($request);
+
+        //Si le formulaire est envoyer et est valide.
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $originalgetting=$form->getNormData()->getPath('originalName');
+            $originalname=$originalgetting->getClientOriginalName();
+            
+            $file = $gedfiles->getPath();
+            
+            $type = $file->guessExtension();
+
+            $fileName = md5(uniqid()).'.'.$file->guessExtension();
+
+            $pathDir = $this->container->getParameter('kernel.root_dir').'/../web/uploads';
+            $file->move($pathDir, $fileName);
+
+            $gedfiles->setType($type);
+            $gedfiles->setPath($fileName);
+            $gedfiles->setIdowner($user->getId());
+            $gedfiles->setIdCategory(1);
+            $gedfiles->setDate( new DateTime());
+            $gedfiles->setOriginalname($originalname);
+
+            $em->persist($gedfiles);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->set('success', 'Fichier envoyé');
+
+            return $this->redirectToRoute('ged_homepage');
+
+        }
+
+        //on recupere tous les fichiers mis en fav par l'user
+        $listfavs=$em->getRepository('GedBundle:Linkbookmark')->findBy(  
+            array('iduser' => $iduser), // Critere
+            array('id' => 'desc')       // Tri
+        );
+
+        foreach ($listfavs as $onefav ) {
+            //on assigne à fav la ligne du fichier dans gedfiles
+            $idfav=$onefav->getIdfile();    
+            $fav = $em->getRepository('GedBundle:Gedfiles')->findOneById($idfav);
+
+            //trouver le nom du fichier
+            $path=$fav->getPath();
+
+            //trouver le type du fichier
+            $type=$fav->getType();
+            $date=$fav->getDate();
+            $name=$fav->getOriginalName();
+
+            $category=$em->getRepository('GedBundle:Category')->findOneById($fav->getIdcategory())->getName();
+
+            //on recupere tous les tags correspondants au fichier
+            $linktag = $em->getRepository('GedBundle:Linktag')->findByIdfile($idfav);
+            $tagnames = [];
+            foreach ($linktag as $tag) {
+                //on recupere l'id du premier tag
+                $idtag=$tag->getIdtag();
+                //on recupere la ligne de la table Gedtag correspondante à l'id d'au dessus
+                $infostag=$em->getRepository('GedBundle:Gedtag')->findOneById($idtag);
+                //on recupere le nom du tag et on met tout ca dans un tableau
+                $tagname=$infostag->getName();
+                $tagnames[]=array(
+                    'id'=>$idtag,
+                    'name'=>$tagname,
+                    );
+                //on fout tout dans un tableau et on a des favoris tout neufs
+            }
+            if(empty($tagnames))
+            {
+                $tagnames=1;
+            }
+            $tabfav[]=array(
+                "idfile"=>$idfav,
+                "tagnames"=>$tagnames,
+                "path"=>$path,
+                "type"=>$type,
+                "category"=>$category,
+                "date"=>$date,
+                "name"=>$name
+                );
+        }
+        if(empty($tabfav))
+        {
+            $tabfav=1;
+        }
+
+        return $this->render('GedBundle::bookmark.html.twig',array(
+                                                                    'categories' => $categories,
+                                                                    'categoryTab'=> $categoryTab,
+                                                                    'form' => $form->createView(),
+                                                                    'user'=>$user,
+                                                                    'tabfav'=>$tabfav,
+                                                                ));
     }
 }
