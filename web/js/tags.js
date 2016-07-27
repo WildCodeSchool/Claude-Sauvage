6afd98d39211556c20eb52b52b952121678aa6bd
Gugusteh <augustin.lefebvre83@gmail.com>
2016-07-27 15:44:17 +0200

    tag ajax functionnal and style done on parameters and editgroup

--- web/js/tags.js
+++ web/js/tags.js
@@ -1,48 +1,62 @@
 $(document).ready(function(){
 	var counttag= $('.counttag').attr('id');
-	console.log(counttag);
 
-	$('.tagicon').click(function(){
-		var idtag = $(this).attr('id');
-		$.ajax({
-			type: 'POST',
-			url: tagremove,
-			data: {idtag: idtag},
-			dataType : 'json',
+	if(isdeleted == 0)
+	{
+		$('.tagicon').click(function(){
+			var idtag = $(this).attr('id');
+			$.ajax({
+				type: 'POST',
+				url: tagremove,
+				data: {idtag: idtag},
+				dataType : 'json',
+			});
+			$("#"+idtag).addClass("tagdisplay");
+			$("#addtagfield").removeAttr('disabled');
+			counttag --;
 		});
-		$("#"+idtag).addClass("tagdisplay");
-		$("#addtagfield").removeAttr('disabled');
-		counttag --;
-	});
+	}
+	else
+	{
+		isdeleted=1;
+	}
 	if( counttag<=2)
 	{
 		$("#addtagfield").removeAttr('disabled');
 		$('#addtagbtn').click(function(){
 			var content = $('#addtagfield').val();
-			console.log(content);
+			counttag++;
 			$.ajax({
 				type: 'POST',
 				url: tagadd,
 				data: {idfile: idfile, content: content},
 				dataType : 'json',
 				success: function(data){
-					console.log(data.tabtag.idtag);
 					$("#taglist").append($('<p>',{ 'id': data.tabtag.idtag }));
 					$("#"+data.tabtag.idtag).text(data.tabtag.name);
 					$("#"+data.tabtag.idtag).append($('<span>',{'class': 'tagicon glyphicon glyphicon-remove', 'id': data.tabtag.idtag}));
 					$("#"+data.tabtag.idtag).addClass('tag');
-					
 					$('.tagicon').click(function(){
-						var idtag = $(this).attr('id');
-						$.ajax({
-							type: 'POST',
-							url: tagremove,
-							data: {idtag: idtag},
-							dataType : 'json',
-						});
-						$("#"+idtag).addClass("tagdisplay");
-						$("#addtagfield").removeAttr('disabled');
-						counttag --;
+						
+						if(isdeleted==0)
+						{
+							var idtag = $(this).attr('id');
+							$.ajax({
+								type: 'POST',
+								url: tagremove,
+								data: {idtag: idtag},
+								dataType : 'json',
+							});
+							$("#"+idtag).addClass("tagdisplay");
+							$("#addtagfield").removeAttr('disabled');
+							counttag --;
+							isdeleted=1;
+						}
+						else
+						{
+							isdeleted=0;
+							return;
+						}
 					});
 				},
 
